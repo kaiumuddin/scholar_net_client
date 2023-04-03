b83ef7776ca1507bacd2b86337caa67f422629c8
Mahfuz Alam <97233771+mahfuz-alam0@users.noreply.github.com>
2023-04-03 15:38:19 +0600

    follow done

--- src/Components/Home/RightContent/Follow/Follow.jsx
+++ src/Components/Home/RightContent/Follow/Follow.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import follow from '../../../../Assets/friend-avatar.jpg';
+import follow2 from '../../../../Assets/friend-avatar2.jpg';
+import follow3 from '../../../../Assets/friend-avatar3.jpg';
+import follow4 from '../../../../Assets/friend-avatar4.jpg';
+import follow5 from '../../../../Assets/friend-avatar5.jpg';
 
 const Follow = () => {
     return (
-        <div>
-            folow
+        <div className='mb-10'>
+            <div className='p-5 border-gray-300 bg-gray-50 rounded-md border'>
+                <p className='font-semibold text-gray-600 pb-5 text-xl'>Who's follownig</p>
+                <div>
+                    <div className='flex items-center gap-3 w-full mb-5'>
+                        <img class="w-10 h-10 rounded-full" src={follow} alt="Rounded avatar" />
+                        <div className='flex w-full items-center justify-between'>
+                            <div>
+                                <p className='font-semibold text-gray-600 cursor-pointer hover:text-sky-500 duration-300'>Kelly Bill</p>
+                                <p className='text-gray-600 text-xs'>Dept colleague</p>
+                            </div>
+                            <p className='text-sky-500'>Follow</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 w-full mb-5'>
+                        <img class="w-10 h-10 rounded-full" src={follow2} alt="Rounded avatar" />
+                        <div className='flex w-full items-center justify-between'>
+                            <div>
+                                <p className='font-semibold text-gray-600 cursor-pointer hover:text-sky-500 duration-300'>Issabel</p>
+                                <p className='text-gray-600 text-xs'>Dept colleague</p>
+                            </div>
+                            <p className='text-sky-500'>Follow</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 w-full mb-5'>
+                        <img class="w-10 h-10 rounded-full" src={follow3} alt="Rounded avatar" />
+                        <div className='flex w-full items-center justify-between'>
+                            <div>
+                                <p className='font-semibold text-gray-600 cursor-pointer hover:text-sky-500 duration-300'>Andrew</p>
+                                <p className='text-gray-600 text-xs'>Dept colleague</p>
+                            </div>
+                            <p className='text-sky-500'>Follow</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 w-full mb-5'>
+                        <img class="w-10 h-10 rounded-full" src={follow4} alt="Rounded avatar" />
+                        <div className='flex w-full items-center justify-between'>
+                            <div>
+                                <p className='font-semibold text-gray-600 cursor-pointer hover:text-sky-500 duration-300'>Sophia</p>
+                                <p className='text-gray-600 text-xs'>Dept colleague</p>
+                            </div>
+                            <p className='text-sky-500'>Follow</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-3 w-full'>
+                        <img class="w-10 h-10 rounded-full" src={follow5} alt="Rounded avatar" />
+                        <div className='flex w-full items-center justify-between'>
+                            <div>
+                                <p className='font-semibold text-gray-600 cursor-pointer hover:text-sky-500 duration-300'>Allen</p>
+                                <p className='text-gray-600 text-xs'>Dept colleague</p>
+                            </div>
+                            <p className='text-sky-500'>Follow</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
