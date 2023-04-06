6d07a1f480fca25c0f7db1e99f06f465c58d0331
kaiumuddin <kaium.uddin2909@gmail.com>
2023-04-06 17:51:53 +0600

    fixed some issues

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -4,13 +4,15 @@ import RightContent from "../RightContent/RightContent";
 
 const Home = () => {
   return (
-<>
-    <div className="grid grid-cols-3 gap-12 container mx-auto max-w-screen-lg my-12">
-      <div className="col-span-2">
-        <LeftContent></LeftContent></div>
-      <div><RightContent></RightContent></div>
-    </div>
-</>
+    <>
+      <div className="grid grid-cols-3 gap-12 container mx-auto max-w-screen-lg my-12">
+        <div className="col-span-2">
+          <LeftContent></LeftContent></div>
+        <div className="">
+          <RightContent></RightContent>
+        </div>
+      </div>
+    </>
   );
 };
 
