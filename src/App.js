af72b0dd5187df86a8c75bb45b7db0accf3e1669
subrota <subrota45278@gmail.com>
2023-04-07 02:40:37 +0600

    add code without toast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div>
       <RouterProvider router={router} />
-      <ToastContainer />
     </div>
   );
 }
