ca9cb488cccb06be457a5ebd0c7a9d689670b326
subrota <subrota45278@gmail.com>
2023-04-06 16:45:21 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <div>
+    <>
+       <ToastContainer />
       <RouterProvider router={router} />
-    </div>
+    </>
   );
 }
 
